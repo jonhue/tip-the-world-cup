aab2682571012e393bf804c6786865f6a1252cf2
Jonas Hübotter <me@jonhue.me>
2018-05-26 17:27:54 +0200

    add matches

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,93 @@ Nation.all.each do |nation|
   name = nation.name.downcase.gsub(' ', '_')
   nation.flag.attach(io: File.open(Rails.application.root.join('db', 'resources', 'flags', "#{name}.png")), filename: "#{name}.png", content_type: 'image/png') unless nation.flag.attached?
 end
+
+
+Match.find_or_create_by!(home: russia, away: saudi_arabia, begins_at: '14 Jun 2018 - 15:00'.to_datetime)
+
+Match.find_or_create_by!(home: egypt, away: uruguay, begins_at: '15 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: morocco, away: ir_iran, begins_at: '15 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: portugal, away: spain, begins_at: '15 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: france, away: australia, begins_at: '16 Jun 2018 - 10:00'.to_datetime)
+Match.find_or_create_by!(home: argentina, away: iceland, begins_at: '16 Jun 2018 - 13:00'.to_datetime)
+Match.find_or_create_by!(home: peru, away: denmark, begins_at: '16 Jun 2018 - 16:00'.to_datetime)
+Match.find_or_create_by!(home: croatia, away: nigeria, begins_at: '16 Jun 2018 - 19:00'.to_datetime)
+
+Match.find_or_create_by!(home: costa_rica, away: serbia, begins_at: '17 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: germany, away: mexico, begins_at: '17 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: brazil, away: switzerland, begins_at: '17 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: sweden, away: korea_republic, begins_at: '18 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: belgium, away: panama, begins_at: '18 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: tunisia, away: england, begins_at: '18 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: columbia, away: japan, begins_at: '19 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: poland, away: senegal, begins_at: '19 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: russia, away: egypt, begins_at: '19 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: portugal, away: morocco, begins_at: '20 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: uruguay, away: saudi_arabia, begins_at: '20 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: ir_iran, away: spain, begins_at: '20 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: denmark, away: australia, begins_at: '21 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: france, away: peru, begins_at: '21 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: argentina, away: croatia, begins_at: '21 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: brazil, away: costa_rica, begins_at: '22 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: nigeria, away: iceland, begins_at: '22 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: serbia, away: switzerland, begins_at: '22 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: belgium, away: tunisia, begins_at: '23 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: korea_republic, away: mexico, begins_at: '23 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: germany, away: sweden, begins_at: '23 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: england, away: panama, begins_at: '24 Jun 2018 - 12:00'.to_datetime)
+Match.find_or_create_by!(home: japan, away: senegal, begins_at: '24 Jun 2018 - 15:00'.to_datetime)
+Match.find_or_create_by!(home: poland, away: columbia, begins_at: '24 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: uruguay, away: russia, begins_at: '25 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: saudi_arabia, away: egypt, begins_at: '25 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: spain, away: morocco, begins_at: '25 Jun 2018 - 18:00'.to_datetime)
+Match.find_or_create_by!(home: ir_iran, away: portugal, begins_at: '25 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: australia, away: peru, begins_at: '26 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: denmark, away: france, begins_at: '26 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: nigeria, away: argentina, begins_at: '26 Jun 2018 - 18:00'.to_datetime)
+Match.find_or_create_by!(home: iceland, away: croatia, begins_at: '26 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: korea_republic, away: germany, begins_at: '27 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: mexico, away: sweden, begins_at: '27 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: serbia, away: brazil, begins_at: '27 Jun 2018 - 18:00'.to_datetime)
+Match.find_or_create_by!(home: switzerland, away: costa_rica, begins_at: '27 Jun 2018 - 18:00'.to_datetime)
+
+Match.find_or_create_by!(home: japan, away: poland, begins_at: '28 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '28 Jun 2018 - 14:00'.to_datetime)
+Match.find_or_create_by!(home: panama, away: tunisia, begins_at: '28 Jun 2018 - 18:00'.to_datetime)
+Match.find_or_create_by!(home: england, away: belgium, begins_at: '28 Jun 2018 - 18:00'.to_datetime)
+
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '30 Jun 2018 - 15:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '30 Jun 2018 - 19:00'.to_datetime)
+#
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '1 Jul 2018 - 15:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '1 Jul 2018 - 19:00'.to_datetime)
+#
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '2 Jul 2018 - 15:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '2 Jul 2018 - 19:00'.to_datetime)
+#
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '3 Jul 2018 - 15:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '3 Jul 2018 - 19:00'.to_datetime)
+#
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '6 Jul 2018 - 15:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '6 Jul 2018 - 19:00'.to_datetime)
+#
+# Match.find_or_create_by!(home: japan, away: poland, begins_at: '7 Jul 2018 - 16:00'.to_datetime)
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '7 Jul 2018 - 19:00'.to_datetime)
+
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '10 Jul 2018 - 19:00'.to_datetime)
+
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '11 Jul 2018 - 19:00'.to_datetime)
+
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '14 Jul 2018 - 15:00'.to_datetime)
+
+# Match.find_or_create_by!(home: senegal, away: columbia, begins_at: '15 Jul 2018 - 16:00'.to_datetime)
