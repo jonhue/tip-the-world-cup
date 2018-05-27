c4320ea8ba9ae1d7ab3ea6733f5f4bb90612b8e8
Jonas Hübotter <me@jonhue.me>
2018-05-27 20:47:17 +0200

    minor bugfixes

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -15,7 +15,7 @@ class MatchesController < ApplicationController
   # GET /app/1/matches/1
   def show
     authorize! :read, @match
-    @tips = @match.tips.sort_by { |tip| tip.earned_points }.reverse
+    @tips = @match.tips.includes(:participant).where(participants: { game: @game }).sort_by { |tip| tip.earned_points }.reverse
     turbolinks_animate 'fadeinright'
     render layout: 'back'
   end
