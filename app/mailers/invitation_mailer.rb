7ce9409fbb221987b7c710951777b5c8228360f6
Jonas Hübotter <me@jonhue.me>
2018-05-30 07:49:15 +0200

    fix invitation mail

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,8 @@
 class InvitationMailer < ApplicationMailer
   def invitation sender, invitation
     @user = User.find_by(email: invitation.email)
+    @sender = sender
     @invitation = invitation
-    mail(to: invitation.email, subject: "#{sender.name} invited you to tip the 2018 World Cup")
+    mail(to: invitation.email, subject: "#{@sender.name} invited you to tip the 2018 World Cup")
   end
 end
