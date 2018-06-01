e16136eac94103b9548422f1c1f8834e2336e469
Jonas Hübotter <me@jonhue.me>
2018-06-01 18:31:14 +0200

    add welcome#index layout

---
+++ app/helpers/welcome_helper.rb
@@ -0,0 +1,9 @@
+module WelcomeHelper
+  def welcome_subtitle
+    if Date.today < Match.all.order(:begins_at).first.begins_at
+      'Start tipping before the 16th of June.'
+    else
+      'The tournament has already begun.'
+    end
+  end
+end
