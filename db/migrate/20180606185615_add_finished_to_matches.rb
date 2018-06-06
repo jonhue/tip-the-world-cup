f0461aeef68d3ef844f186f71266d243371e11bc
Jonas Hübotter <me@jonhue.me>
2018-06-06 21:28:25 +0200

    cleanup migrations

--- db/migrate/20180606185615_add_finished_to_matches.rb
+++
@@ -1,6 +0,0 @@
-class AddFinishedToMatches < ActiveRecord::Migration[5.2]
-  def change
-    add_column :matches, :finished, :boolean, null: false, default: false
-    remove_column :matches, :live
-  end
-end
