b7e3a25f587e3bf6559fd0bb9447f83d38b50423
Jonas Hübotter <me@jonhue.me>
2018-06-08 13:51:38 +0200

    cleanup

--- db/migrate/20180608114116_add_penalties_to_matches.rb
+++
@@ -1,6 +0,0 @@
-class AddPenaltiesToMatches < ActiveRecord::Migration[5.2]
-  def change
-    add_column :matches, :home_penalties, :integer
-    add_column :matches, :away_penalties, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_185615) do
+ActiveRecord::Schema.define(version: 2018_06_08_114116) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 2018_06_06_185615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "finished", default: false, null: false
+    t.integer "home_penalties"
+    t.integer "away_penalties"
     t.index ["away_id"], name: "index_matches_on_away_id"
     t.index ["home_id"], name: "index_matches_on_home_id"
   end
