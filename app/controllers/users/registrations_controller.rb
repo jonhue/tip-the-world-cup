d1e292f5d00df2664ec67542df67a5b3060080e6
Jonas Hübotter <me@jonhue.me>
2018-06-09 18:52:43 +0200

    minor bugfixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   layout 'back', only: [:edit]
 
