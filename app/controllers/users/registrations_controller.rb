0995b2e9d376b6e1e4fa85b09c1b87db65008592
Jonas Hübotter <me@jonhue.me>
2018-06-09 18:57:32 +0200

    minor bugfixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,9 +50,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
