3eff3b67d11a9f7c8f21ee10b9d073d3a5f4a6c2
Jonas Hübotter <me@jonhue.me>
2018-06-09 12:29:22 +0200

    add locale column to users

--- db/migrate/20180525182257_devise_create_users.rb
+++ db/migrate/20180525182257_devise_create_users.rb
@@ -33,6 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
+      t.string :locale
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_114116) do
+ActiveRecord::Schema.define(version: 2018_06_09_102837) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_114116) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "locale"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
