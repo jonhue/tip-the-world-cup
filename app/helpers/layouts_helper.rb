917d6fd90fe7da34382fb8c22e136506ddc0ffd7
Jonas Hübotter <me@jonhue.me>
2018-08-04 19:41:21 +0200

    [#72] Space on top of games#index

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module LayoutsHelper
-  def body_class(custom_class)
-    'myg myg-layout myg-layout--toolbar '\
+  def body_class
+    'myg myg-layout '\
     "#{class_hierarchy([params[:controller].split('/'), action_name])} "\
-    "#{custom_class}"
+    "#{area(:body_class)}"
   end
 end
