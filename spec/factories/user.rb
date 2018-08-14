0b13c782e217e6e59c1acfd73e0d01b30738bd38
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-08-14 22:07:26 +0200

    Update user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,13 +2,13 @@
 
 FactoryBot.define do
   factory :user do
-    email 'user@example.com'
-    password 'password'
-    name 'User'
+    email { 'user@example.com' }
+    password { 'password' }
+    name { 'User' }
   end
   factory :another_user, class: User do
-    email 'another_user@example.com'
-    password 'password'
-    name 'Another User'
+    email { 'another_user@example.com' }
+    password { 'password' }
+    name { 'Another User' }
   end
 end
